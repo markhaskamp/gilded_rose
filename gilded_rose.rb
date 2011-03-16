2318ace3e56ff437472d6a175914c0891263f7fc
Mark Haskamp <mrhask@gmail.com>
2011-03-15 23:15:40 -0400

    removed Backstage code from old code

--- gilded_rose.rb
+++ gilded_rose.rb
@@ -81,39 +81,17 @@ def update_quality(items)
       item.sell_in = i.get_sell_in
     else
 
-    if item.name != 'Backstage passes to a TAFKAL80ETC concert'
       if item.quality > 0
         item.quality -= 1
       end
-    else
-      if item.quality < 50
-        item.quality += 1
-        if item.name == 'Backstage passes to a TAFKAL80ETC concert'
-          if item.sell_in < 11
-            if item.quality < 50
-              item.quality += 1
-            end
-          end
-          if item.sell_in < 6
-            if item.quality < 50
-              item.quality += 1
-            end
-          end
+
+      item.sell_in -= 1
+
+      if item.sell_in < 0
+        if item.quality > 0
+          item.quality -= 1
         end
       end
-    end
-
-    item.sell_in -= 1
-
-    if item.sell_in < 0
-        if item.name != 'Backstage passes to a TAFKAL80ETC concert'
-          if item.quality > 0
-            item.quality -= 1
-          end
-        else
-          item.quality = item.quality - item.quality
-        end
-    end
     end
   end
 end
